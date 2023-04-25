d998ec90e407027deee0b8a2a27f6baea4f75af8
rasmus johansson <rajoh@sdfe.dk>
2023-04-25 14:16:11 +0200

    when creating all.txt we can now filter away any files that dont exists in all different data_source folders e.g splited_rgb splitted_DSM

--- src/befaestelse_dataset_creation/create_txt_files.py
+++ src/befaestelse_dataset_creation/create_txt_files.py
@@ -11,9 +11,10 @@ def main(config):
     valid_txt_filename =ini_parser[section]["valid_txt_filename"]
     path_to_images =ini_parser[section]["splitted_mask_folder"]
     datatype =ini_parser[section]["datatype"]
+    other_data_folders = ["splitted_"+ data_folder for data_folder in ini_parser[section]["datatypes"]]
     nr_of_images_between_validation_samples =int(ini_parser[section]["nr_of_images_between_validation_samples"])
     print("creating all.txt and valid.txt")
-    create_all_and_valid_txt.create_all_and_valid(all_txt_filename =all_txt_filename,valid_txt_filename=valid_txt_filename,path_to_training_images=path_to_images,datatype=datatype,nr_of_images_between_validation_samples=nr_of_images_between_validation_samples)
+    create_all_and_valid_txt.create_all_and_valid(all_txt_filename =all_txt_filename,valid_txt_filename=valid_txt_filename,path_to_training_images=path_to_images,datatype=datatype,nr_of_images_between_validation_samples=nr_of_images_between_validation_samples,other_data_folders=other_data_folders)
     print("creating train.txt")
     create_train_txt.create_train_txt(path_to_all_txt=all_txt_filename,path_to_valid_txt=valid_txt_filename,name_prefix="")
 
